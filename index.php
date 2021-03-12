089a9af9a6a3c92d4f905d001c62f3e5677b8414
MikeTheulen <67059331+MikeTheulen@users.noreply.github.com>
2021-03-12 21:20:40 +0100

    Final update for Server Update
    Met dit kan je via de website de files updaten

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
+<!-- NIET VERWIJDEREN -->
 <?php
+  if (isset($_GET['update'])) {
+    exec('auto_update.sh');
+	sleep(4);
+	echo '<meta http-equiv=Refresh content="0;url=index.php">';
+  }
+?>
+<a href='index.php?update=true'>Update Server</a>
+<!-- NIET VERWIJDEREN -->
 
-echo "Dit is een test en dit is nieuw";
-
+<?php
+	echo "Dit is een test en dit is nieuw";
 ?>
